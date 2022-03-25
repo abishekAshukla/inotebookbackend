b48b68868442415ea9863245613bb07d2e7d6de3
Abhishek Shukla <90856267+abhishek2002Git@users.noreply.github.com>
2022-03-25 18:08:22 +0530

    sd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = 5000
 
 app.get('/express', (req, res) => { res.send('Hello from Express!')})
-app.get('/node', (req, res) => { res.send('Hello from NodeJs yuhu machate raho tony stark urff ironman')})
+app.get('/node', (req, res) => { res.send('Hello from NodeJs yuhu machate raho tony stark urff batman')})
 
 app.use(cors())
 app.use(express.json())
@@ -27,5 +27,48 @@ app.use('/api/message', require('./routes/message'))
 // })
 
 const server = app.listen(process.env.PORT || port, console.log(`server started on ${port}`))
+
+const io = require('socket.io')(server , {
+  pingTimeout: 60000,
+  cors: {
+      origin: "http://localhost:3000",
+  },
+})
+
+io.on("connection" , (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup" , (userData) => {
+      socket.join(userData._id);
+      console.log(userData._id);
+      socket.emit("connected")
+  })
+
+  socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"))  
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"))  
+
+   socket.on("new message" , (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+
+  if (!chat.users) return console.log("chat.users not defined");
+
+  chat.users.forEach((user) => {
+    if (user._id == newMessageRecieved.sender._id) return;
+
+    socket.in(user._id).emit("message recieved", newMessageRecieved);
+  });
+   }) 
+
+  //  very importnat step, closing the connection to save the bandwidth
+   socket.off("setup", () => {
+       console.log("USER DISCONNECTED");
+       socket.leave(userData._id)
+   })
+})
 
 
